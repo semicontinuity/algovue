6c6561ac6bc21e1c2f2e678e81aabcfdf51eefd7
semicontinuity <igor.a.karpov@gmail.com>
2020-07-23 22:56:50 +0300

    kmp

--- v1/algo/knuth_morris_pratt.js
+++ v1/algo/knuth_morris_pratt.js
@@ -1,12 +1,16 @@
 test = function() {
 
-    const computeLPSArray = vm.functionDeclaration(
+    let computeLPSArray;
+    let kmp;
+    computeLPSArray = vm.functionDeclaration(
         'computeLPSArray',
         [vm.variable('pat'), vm.variable('M'), vm.variable('lps')],
         vm.sequenceStatement([
+            vm.lineComment('// Preprocess the pattern (calculate lps[] array)'),
             vm.assignment(vm.varWrite('len'), vm.number(0)),
-            vm.assignment(vm.varWrite('i'), vm.number(1)),
+            vm.assignment(vm.varWrite('i', 'pat'), vm.number(1)),
             vm.assignment(vm.arrItemWrite('lps', vm.number(0)), vm.number(0)),
+            vm.lineComment(),
             vm.whileStatement(
                 vm.expression(vm.lt(), vm.variable('i'), vm.variable('M')),
                 vm.sequenceStatement([
@@ -34,45 +38,50 @@ test = function() {
             )
         ])
     );
-    const kmp = vm.functionDeclaration(
+    kmp = vm.functionDeclaration(
         'kmp',
         [vm.variable('txt'), vm.variable('N'), vm.variable('pat'), vm.variable('M'), vm.variable('lps')],
         vm.sequenceStatement([
-            vm.assignment(vm.varWrite('j'), vm.number(0)),
+            vm.ifStatement(
+                vm.expression(vm.le(), vm.variable('M'), vm.number(0)),
+                vm.returnStatement(vm.number(-1))
+            ),
             vm.assignment(undefined, vm.functionCall(computeLPSArray, [vm.variable('pat'), vm.variable('M'), vm.variable('lps')])),
-            vm.assignment(vm.varWrite('i'), vm.number(0)),
+            vm.assignment(vm.varWrite('j', 'pat'), vm.number(0)),
+            vm.assignment(vm.varWrite('i', 'txt'), vm.number(0)),
+            vm.lineComment(),
             vm.whileStatement(
                 vm.expression(vm.lt(), vm.variable('i'), vm.variable('N')),
                 vm.sequenceStatement([
                     vm.ifStatement(
                         vm.expression(vm.eq(), vm.arrItem('pat', vm.variable('j')), vm.arrItem('txt', vm.variable('i'))),
                         vm.sequenceStatement([
+                            vm.lineComment('// Match at position j'),
                             vm.assignment(vm.varWrite('j'), vm.expression(vm.plus(), vm.variable('j'), vm.number(1))),
-                            vm.assignment(vm.varWrite('i'), vm.expression(vm.plus(), vm.variable('i'), vm.number(1)))
-                        ])
-                    ),
-                    vm.ifStatement(
-                        vm.expression(vm.eq(), vm.variable('j'), vm.variable('M')),
-                        vm.sequenceStatement([
-                            vm.returnStatement(vm.expression(vm.minus(), vm.variable('i'), vm.variable('j')))
+                            vm.assignment(vm.varWrite('i'), vm.expression(vm.plus(), vm.variable('i'), vm.number(1))),
+                            vm.ifStatement(
+                                vm.expression(vm.eq(), vm.variable('j'), vm.variable('M')),
+                                vm.sequenceStatement([
+                                    vm.returnStatement(vm.expression(vm.minus(), vm.variable('i'), vm.variable('j')))
+                                ])
+                            )
                         ]),
-                        vm.ifStatement(
-                            vm.expression(vm.and(), vm.expression(vm.lt(), vm.variable('i'), vm.variable('N')), vm.expression(vm.ne(), vm.arrItem('pat', vm.variable('j')), vm.arrItem('txt', vm.variable('i')))),
-                            vm.sequenceStatement([
-                                vm.ifStatement(
-                                    vm.expression(vm.ne(), vm.variable('j'), vm.number(0)),
-                                    vm.sequenceStatement([
-                                        vm.assignment(vm.varWrite('j'), vm.arrItem('lps', vm.expression(vm.minus(), vm.variable('j'), vm.number(1))))
-                                    ]),
-                                    vm.sequenceStatement([
-                                        vm.assignment(vm.varWrite('i'), vm.expression(vm.plus(), vm.variable('i'), vm.number(1)))
-                                    ])
-                                )
-                            ])
-                        )
+                        vm.sequenceStatement([
+                            vm.lineComment('// Mismatch at position j'),
+                            vm.ifStatement(
+                                vm.expression(vm.le(), vm.variable('j'), vm.number(0)),
+                                vm.sequenceStatement([
+                                    vm.assignment(vm.varWrite('i'), vm.expression(vm.plus(), vm.variable('i'), vm.number(1)))
+                                ]),
+                                vm.sequenceStatement([
+                                    vm.assignment(vm.varWrite('j'), vm.arrItem('lps', vm.expression(vm.minus(), vm.variable('j'), vm.number(1))))
+                                ])
+                            )
+                        ])
                     )
                 ])
             ),
+            vm.lineComment(),
             vm.returnStatement(vm.number(-1))
         ])
     );
